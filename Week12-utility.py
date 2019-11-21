69e3afc5a3eb5902126539c5ad146b292bba30ca
Jakob Pigati <jpigati@mymail.mines.edu>
2019-11-21 10:43:32 -0700

    added ScoreFinder

--- Week12-utility.py
+++ Week12-utility.py
@@ -9,3 +9,12 @@ def UpdateString(string, string_add, index): #doesnt replace just adds
 def FindWordCount(List, string):
     count = List.count(string)
     return count
+def ScoreFinder(players, scores, person):
+    score = False
+    for i in range(len(players)):
+        if players[i].lower() == person.lower():
+            score = scores[i]
+    if score == False:
+        print('OUTPUT player not found')
+    else:
+        print('OUTPUT', person, 'got a score of', score)
